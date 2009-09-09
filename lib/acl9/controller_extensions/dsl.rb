19678cc264927e3ee952bf86bdfd11e337d8cc60
oleg dashevskii <olegdashevskii@gmail.com>
2009-09-09 17:42:51 +0700

    Further refactoring DSL processor. Added test & docs.
    actions block still unsupported.

--- lib/acl9/controller_extensions/dsl.rb
+++ lib/acl9/controller_extensions/dsl.rb
@@ -1,31 +1,68 @@
 module Acl9
   module Dsl
     def expression_join(parts, with)
-      parts.map { |p| "(#{p})" } * with
+      # TODO: remove excessful parentheses
+      parts.compact.map { |p| "(#{p})" } * with
     end
 
+    # Construct AND expression from parts
+    #
+    # @param [Array] parts Array of expressions
     def andify(parts)
+      # TODO: remove (true)
       expression_join(parts, ' && ')
     end
 
+    # Construct OR expression from parts
+    #
+    # @param [Array] parts Array of expressions
     def orify(parts)
+      # TODO: remove (false)
       expression_join(parts, ' || ')
     end
 
     module_function :expression_join, :andify, :orify
 
-    # A class that represents :if/:unless rule conditions
+    # :if or :unless rule conditions
     class Condition < Struct.new(:method, :negate)
-      # @return [String] Condition-checking expression (which will return +true+ if condition is met)
+      # @return [String] Condition-checking expression (which will return +true+ if condition is met). Will return +nil+ if method is +nil+.
       # @param [Generator] gen Generator
       def expression(gen)
-        m = gen.method_call(method)
-        negate ? "!#{m}" : m
+        if method
+          m = gen.method_call(method)
+          negate ? "!#{m}" : m
+        end
       end
     end
 
-    # A class that represents :to/:except action checks
+    # Combined :if and :unless condition
+    #
+    # Sorta IF && !UNLESS
+    class DoubleCondition
+      # @param [Symbol] _if Method that should return trueish value for a rule to get checked
+      # @param [Symbol] _unless Method that should return +false+ or +nil+ for a rule to get checked
+      def initialize(_if, _unless)
+        @if = Condition.new(_if, false)
+        @unless = Condition.new(_unless, true)
+      end
+
+      # @return [String] Condition-checking expression. Will return +nil+ when both IF and UNLESS conditions are not specified.
+      # @param [Generator] gen Generator
+      def expression(gen)
+        res = Dsl.andify [@if.expression(gen), @unless.expression(gen)]
+
+        res.empty? ? nil : res
+      end
+    end
+
+    # :to/:except action checks
     class ActionCheck < Struct.new(:actions, :negate)
+      def initialize(*args)
+        super
+
+        self.actions = [actions] unless actions.is_a? Array
+      end
+
       # @return [String] Action-checking expression (which will return +true+ if current action should
       # be checked)
       # @param [Generator] gen Generator
@@ -36,7 +73,7 @@ module Acl9
             else
               set_of_actions = "Set.new([" + actions.map { |act| "'#{act}'"}.join(',')  + "])"
 
-              "#{set_of_actions}.include?(#{_action_ref})"
+              "#{set_of_actions}.include?(#{gen.current_action})"
             end
 
         negate ? "!#{m}" : m
@@ -47,7 +84,7 @@ module Acl9
     ANONYMOUS = nil
     ALL       = true
 
-    # A class that represents access control rule
+    # Access control rule
     class Rule < Struct.new(:roles, :object, :action_check, :condition)
       # @return [String] Rule-matching expression (which will return +true+ if rule matched)
       # @param [Generator] gen Generator
@@ -63,13 +100,11 @@ module Acl9
           end
         end.uniq
 
-        role_checks = nil if role_checks == ['true']
-
         Dsl.andify([
                     action_check && action_check.expression(gen),
                     condition && condition.expression(gen),
                     role_checks && Dsl.orify(role_checks)
-                   ].compact)
+                   ])
       end
 
       protected
@@ -88,11 +123,13 @@ module Acl9
       end
     end
 
+    # Allow rule
     class AllowRule < Rule
       def allow?; true  end
       def deny?;  false end
     end
 
+    # Deny rule
     class DenyRule < Rule
       def allow?; false end
       def deny?;  true  end
@@ -100,6 +137,7 @@ module Acl9
 
     # DSL processor
     class Processor
+      # Array of rules
       attr_reader :rules
 
       def initialize
@@ -107,17 +145,30 @@ module Acl9
         @rules = []
       end
 
-      # Populate rules from given block
+      # Populate {#rules} using given block
+      #
+      # @yield Block with ACL instructions
+      # @see #default
+      # @see #allow
+      # @see #deny
       def acl_block!(&block)
         instance_eval(&block)
       end
 
+      # Returns default action
+      #
       # @return [Symbol] default action (:allow or :deny)
+      # @see #default
       def default_action
         @default_action.nil? ? :deny : @default_action
       end
 
-      # @return [String] Block converted to Ruby expression (which will return +true+ if access allowed)
+      # Translate {#rules} into Ruby expression, taking {#default_action} into consideration
+      #
+      # @return [String] Ruby expression (which will return +true+ if access allowed)
+      # @see #rules
+      # @see #default
+      # @see #default_action
       def expression(gen)
         allow_rules = rules.select { |rule| rule.allow? }
         deny_rules  = rules.select { |rule| rule.deny? }
@@ -129,7 +180,7 @@ module Acl9
                        end
 
         not_denied_expr = if deny_rules.size > 0
-                            "!" + Dsl.orify(deny_rules.map { |rule| rule.expression(gen) })
+                            "!(#{Dsl.orify(deny_rules.map { |rule| rule.expression(gen) })})"
                           else
                             "true"
                           end
@@ -137,12 +188,12 @@ module Acl9
         Dsl.send((default_action == :deny ? :andify : :orify), [allowed_expr, not_denied_expr])
       end
 
-      # Set default mode.
+      # Set default allow or default deny.
       #
       # There are two modes: default allow and default deny. The "default" case
       # occurs when neither of the rules matched.
       #
-      # Default deny mode is used you don't call @default@ at all.
+      # Default deny mode is used you don't call {#default} at all.
       #
       # @param default_action [Symbol] :allow or :deny
       def default(default_action)
@@ -155,43 +206,179 @@ module Acl9
         @default_action = default_action
       end
 
+      # Add an allow rule
+      #
+      # Rule is cool.
+      #
+      # @example
+      #   allow all
+      #
+      #
+      # @overload allow(*roles, opts={})
+      #   @param [Array] roles A list of roles. Each role is either a Symbol (+:admin+, +:manager+, etc.) or a pseudorole  (+all+, +logged_in+ or +anonymous+.
+      #   @param [Hash] options Rule options
+      #   @option opts [Symbol] :if Method that should return trueish value for the rule to be considered
+      #   @option opts [Symbol] :unless Method that should return +nil+ or +false+ for the rule to be considered
+      #   @option opts [Array<Symbol, String>, Symbol, String] :to Action scope. A list of actions (or single action) the rule will only be considered for.
+      #   @option opts [Array<Symbol, String>, Symbol, String] :except Action scope. A list of actions (or single action) the rule will not be considered for.
+
+      #   @option opts [Symbol, Class] :of Object.
+      #   @option opts [Symbol, Class] :for Synonym for +:of+
+      #   @option opts [Symbol, Class] :in Synonym for +:of+
+      #   @option opts [Symbol, Class] :on Synonym for +:of+
+      #   @option opts [Symbol, Class] :at Synonym for +:of+
+      #   @option opts [Symbol, Class] :by Synonym for +:of+
+      # @see #all
+      # @see #logged_in
+      # @see #anonymous
+      # @see #deny
       def allow(*args)
         rule(AllowRule, *args)
       end
 
+      # Add a deny rule
+      #
+      # The {#deny} method has exactly same semantics as {#allow}. The only difference is that
+      # access is denied when rule is matched, rather than allowed.
+      #
+      # @see #allow
       def deny(*args)
         rule(DenyRule, *args)
       end
 
       def actions(*args, &block)
-        # ...
+        # TODO
+        raise
       end
 
       alias action actions
 
+      # Logged in pseudorole.
+      #
+      # Every user that has successfully logged in has this role. This is the opposite of {#anonymous}.
+      #
+      # @example Allow logged in users to request :index and :show actions in the controller. Access for not logged in (anonymous) users will be denied.
+      #
+      #   class SecretsController < ApplicationController
+      #     access_control do
+      #       allow logged_in, :to => [:index, :show]
+      #     end
+      #
+      #     def index
+      #       # ...
+      #     end
+      #
+      #     def show
+      #       # ...
+      #     end
+      #   end
+      #
+      # @see #anonymous
+      # @see #all
       def logged_in; LOGGED_IN end
+
+      # Anonymous pseudorole.
+      #
+      # Every not logged in user has this role. This is the opposite of {#logged_in}.
+      #
+      # @example Only allow anonymous users to log in.
+      #
+      #   class SecretsController < ApplicationController
+      #     access_control do
+      #       allow anonymous, :to => :login
+      #       allow logged_in, :to => :logout
+      #     end
+      #
+      #     def login
+      #       # ...
+      #     end
+      #
+      #     def logout
+      #       # ...
+      #     end
+      #   end
+      #
+      # @see #logged_in
+      # @see #all
       def anonymous; ANONYMOUS end
 
-      [:all, :everyone, :everybody, :anyone].each do |meth|
-        define_method(meth) { ALL }
-      end
+      # "All" pseudorole.
+      #
+      # Every user, either logged in or anonymous, has this role. Primarily used to allow access to actions
+      # that should be widely accessible.
+      #
+      # @example Allow access to all actions
+      #   class OpenController < ApplicationController
+      #     access_control do   # Access control block could be completely omitted here
+      #       allow all
+      #     end
+      #
+      #     # ...
+      #   end
+      #
+      # @example Readonly access
+      #   class ProductsController < ApplicationController
+      #     access_control do
+      #       allow all, :to => [:index, :show]
+      #       allow :admin                       # admin can also create, edit and destroy products
+      #     end
+      #
+      #     # ...
+      #   end
+      def all; ALL end
+
+      alias everyone all
+      alias everybody all
+      alias anyone all
 
       private
 
-      def rule(klass, *args)
+      VALID_PREPOSITIONS = %w(of for in on at by).freeze #unless defined? VALID_PREPOSITIONS
+
+      def rule(rule_class, *args)
         options = args.extract_options!
 
+        raise ArgumentError, "allow/deny should have at least 1 argument" if args.empty?
+
+        # 1. Action scope (:to/:except)
         to = options.delete(:to)
         except = options.delete(:except)
 
         action_check = case
                        when to && !except
-                         ActionCheck.new(to, true)
+                         ActionCheck.new(to, false)
                        when !to && except
-                         ActionCheck.new(except, false)
+                         ActionCheck.new(except, true)
                        when to && except
                          raise ArgumentError, "both :to and :except cannot be specified in the rule"
                        end
+
+        # 2. Object (:of/:for/...)
+        object = nil
+
+        VALID_PREPOSITIONS.each do |prep|
+          if options[prep.to_sym]
+            raise ArgumentError, "You may only use one preposition to specify object" if object
+
+            object = options[prep.to_sym]
+          end
+        end
+
+        unless [Class, Symbol, NilClass].any? { |klass| object.is_a? klass }
+          raise ArgumentError, "object specified by preposition can only be a Class or a Symbol"
+        end
+
+        # 3. Rule condition (:if/:unless)
+        _if = options.delete(:if)
+        _unless = options.delete(:unless)
+
+        unless [_if, _unless].all? { |cond| cond.nil? || cond.is_a?(Symbol) }
+          raise ArgumentError, ":if/:unless option value must be a Symbol"
+        end
+
+        condition = DoubleCondition.new(_if, _unless) if (_if || _unless)
+
+        @rules << rule_class.new(args, object, action_check, condition)
       end
     end
   end
